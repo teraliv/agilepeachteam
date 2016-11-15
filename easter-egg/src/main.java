f369a59917482f3baf692fc1e033b1fed040c9d3
sean3740 <sean3740@IT30201462.insttech.washington.edu>
2016-11-15 08:33:06 -0800

    Set up the basic program, and entered my name. Trying to commit/push.

--- easter-egg/src/main.java
+++ easter-egg/src/main.java
@@ -2,7 +2,34 @@
  * Created by aterikov on 11/3/16.
  * Changed by Ahana Ghosh
  */
-public class main { 
-	
-    
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class main {
+
+	public static void main(String[] args)
+	{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		String ans="b";	//initlized to non-a and non-null value
+		while(ans.compareTo("a")!=0)
+		{
+			System.out.println("Enter \"a\" for an easter egg.");
+		    try 
+		    {
+		    	ans = br.readLine();
+		    } 
+		    catch (IOException e) 
+		    {
+				e.printStackTrace();
+		    }
+		}
+		//replace name# with your name
+		System.out.println("Agile Peach Team:\nSean O'Donnell\nName2\nName3\nName4\nName5");
+	}
+
 }
+
+
+
+
